3cc3d5d11cb9d83b0199a7290cf6ce530e7ab1cd
learnWithZak <learn.with.zak@gmail.com>
2022-10-03 05:39:55 +0200

    refactor: Load place details in a toast

--- app/src/main/java/com/zak/placebook/MapsActivity.kt
+++ app/src/main/java/com/zak/placebook/MapsActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.location.*
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -14,13 +15,20 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.PointOfInterest
 import com.zak.placebook.databinding.ActivityMapsBinding
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.net.FetchPlaceRequest
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.zak.placebook.BuildConfig.MAPS_API_KEY
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var map: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var placesClient: PlacesClient
 
     companion object {
         private const val REQUEST_LOCATION = 1
@@ -38,6 +46,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
         setupLocationClient()
+        setupPlacesClient()
     }
 
     /**
@@ -53,8 +62,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         map = googleMap
         getCurrentLocation()
         map.setOnPoiClickListener {
-            Toast.makeText(this, it.name, Toast.LENGTH_LONG).show()
+            displayPoi(it)
         }
+    }
+
+    private fun setupPlacesClient() {
+        Places.initialize(applicationContext, MAPS_API_KEY)
+        placesClient = Places.createClient(this)
     }
 
     private fun setupLocationClient() {
@@ -98,5 +112,26 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 }
             }
         }
+    }
+
+    private fun displayPoi(pointOfInterest: PointOfInterest) {
+        val placeId = pointOfInterest.placeId
+        val placeFields = listOf(Place.Field.ID,
+            Place.Field.NAME,
+            Place.Field.PHONE_NUMBER,
+            Place.Field.PHOTO_METADATAS,
+            Place.Field.ADDRESS,
+            Place.Field.LAT_LNG)
+        val request = FetchPlaceRequest.builder(placeId, placeFields).build()
+        placesClient.fetchPlace(request)
+            .addOnSuccessListener { response ->
+                val place = response.place
+                Toast.makeText(this, "${place.name}, ${place.phoneNumber ?: "Unknown phone number"}", Toast.LENGTH_LONG).show()
+            }.addOnFailureListener { exception ->
+                if (exception is ApiException) {
+                    val statusCode = exception.statusCode
+                    Log.e(TAG, "Place not found: ${exception.message}, statusCode: $statusCode")
+                }
+            }
     }
 }
