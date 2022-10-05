b216ff03d4bbd78f7e8d0d2361e1f047de947966
learnWithZak <learn.with.zak@gmail.com>
2022-10-05 05:50:34 +0200

    refactor: Add Dao

---
+++ app/src/main/java/com/zak/placebook/db/BookmarkDao.kt
@@ -0,0 +1,33 @@
+package com.zak.placebook.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy.IGNORE
+import androidx.room.OnConflictStrategy.REPLACE
+import androidx.room.Query
+import androidx.room.Update
+import com.zak.placebook.model.Bookmark
+
+@Dao
+interface BookmarkDao {
+
+    @Query("SELECT * FROM Bookmark")
+    fun loadAll(): LiveData<List<Bookmark>>
+
+    @Query("SELECT * FROM Bookmark WHERE id = :bookmarkId")
+    fun loadBookmark(bookmarkId: Long): Bookmark
+
+    @Query("SELECT * FROM bookmark WHERE id = :bookmarkId")
+    fun loadLiveBookmark(bookmarkId: Long): LiveData<Bookmark>
+
+    @Insert(onConflict = IGNORE)
+    fun insertBookmark(bookmark: Bookmark): Long
+
+    @Update(onConflict = REPLACE)
+    fun updateBookmark(bookmark: Bookmark)
+
+    @Delete
+    fun deleteBookmark(bookmark: Bookmark)
+}
