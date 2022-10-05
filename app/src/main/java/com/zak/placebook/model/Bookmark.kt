5bb02188fb52b5ede63a23866f8cc0afd3d61bf2
learnWithZak <learn.with.zak@gmail.com>
2022-10-05 05:38:20 +0200

    refactor: Add Room entity

---
+++ app/src/main/java/com/zak/placebook/model/Bookmark.kt
@@ -0,0 +1,15 @@
+package com.zak.placebook.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Bookmark(
+    @PrimaryKey(autoGenerate = true) var id: Long? = null,
+    var placeId: String? = null,
+    var name: String = "",
+    var address: String = "",
+    var latitude: Double = 0.0,
+    var longitude: Double = 0.0,
+    var phone: String = ""
+)
