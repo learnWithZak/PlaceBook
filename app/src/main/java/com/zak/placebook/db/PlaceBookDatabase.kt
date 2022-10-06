24284ae359ee2ec061df55b2f40e3d1a1cc14d51
learnWithZak <learn.with.zak@gmail.com>
2022-10-06 05:21:44 +0200

    refactor: Add a database

---
+++ app/src/main/java/com/zak/placebook/db/PlaceBookDatabase.kt
@@ -0,0 +1,27 @@
+package com.zak.placebook.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.zak.placebook.model.Bookmark
+
+@Database(entities = [Bookmark::class], version = 1)
+abstract class PlaceBookDatabase: RoomDatabase() {
+
+    abstract fun bookmarkDao(): BookmarkDao
+
+    companion object {
+        private var instance: PlaceBookDatabase? = null
+
+        fun getInstance(context: Context): PlaceBookDatabase {
+            if (instance == null) {
+                instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    PlaceBookDatabase::class.java,
+                    "PlaceBook").build()
+            }
+            return instance as PlaceBookDatabase
+        }
+    }
+}
