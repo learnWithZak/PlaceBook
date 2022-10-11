3e8b149d2e91c72042c2a821fd1681e19e7a1ac8
learnWithZak <learn.with.zak@gmail.com>
2022-10-11 06:00:09 +0200

    refactor: Using the intent data

--- app/src/main/java/com/zak/placebook/ui/BookmarkDetailsActivity.kt
+++ app/src/main/java/com/zak/placebook/ui/BookmarkDetailsActivity.kt
@@ -10,12 +10,13 @@ import com.zak.placebook.viewmodel.BookmarkDetailsViewModel
 class BookmarkDetailsActivity : AppCompatActivity() {
     private lateinit var databinding: ActivityBookmarkDetailsBinding
     private val bookmarkDetailsViewModel by viewModels<BookmarkDetailsViewModel>()
-    private val bookmarkDetailsView: BookmarkDetailsViewModel.BookmarkDetailsView? = null
+    private var bookmarkDetailsView: BookmarkDetailsViewModel.BookmarkDetailsView? = null
 
     override fun onCreate(savedInstanceState: android.os.Bundle?) {
         super.onCreate(savedInstanceState)
         databinding = DataBindingUtil.setContentView(this, R.layout.activity_bookmark_details)
         setupToolbar()
+        getIntentData()
     }
 
     private fun setupToolbar() {
@@ -27,6 +28,17 @@ class BookmarkDetailsActivity : AppCompatActivity() {
             val placeImage = bookmarkView.getImage(this)
             placeImage?.let {
                 databinding.imageViewPlace.setImageBitmap(placeImage)
+            }
+        }
+    }
+
+    private fun getIntentData() {
+        val bookmarkId = intent.getLongExtra(MapsActivity.EXTRA_BOOKMARK_ID, 0)
+        bookmarkDetailsViewModel.getBookmark(bookmarkId)?.observe(this) {
+            it?.let {
+                bookmarkDetailsView = it
+                databinding.bookmarkDetailsView = it
+                populateImageView()
             }
         }
     }
