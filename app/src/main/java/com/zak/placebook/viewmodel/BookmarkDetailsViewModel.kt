df2e373cfb93da2a80c2c1e41ddeaf31d7a8e35c
learnWithZak <learn.with.zak@gmail.com>
2022-10-11 05:48:10 +0200

    refactor: Bookmark view model

--- app/src/main/java/com/zak/placebook/viewmodel/BookmarkDetailsViewModel.kt
+++ app/src/main/java/com/zak/placebook/viewmodel/BookmarkDetailsViewModel.kt
@@ -3,12 +3,16 @@ package com.zak.placebook.viewmodel
 import android.app.Application
 import android.content.Context
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
 import com.zak.placebook.model.Bookmark
 import com.zak.placebook.repository.BookmarkRepo
 import com.zak.placebook.util.ImageUtils
 
 class BookmarkDetailsViewModel(application: Application): AndroidViewModel(application) {
+
     private val bookmarkRepo = BookmarkRepo(getApplication())
+    private var bookmarkDetailsView: LiveData<BookmarkDetailsView>? = null
 
     data class BookmarkDetailsView(
         var id: Long? = null,
@@ -20,5 +24,29 @@ class BookmarkDetailsViewModel(application: Application): AndroidViewModel(appli
         fun getImage(context: Context) = id?.let {
             ImageUtils.loadBitmapFromFile(context, Bookmark.generateImageFilename(it))
         }
+    }
+
+    private fun bookmarkToBookmarkView(bookmark: Bookmark): BookmarkDetailsView {
+        return BookmarkDetailsView(
+            id = bookmark.id,
+            name = bookmark.name,
+            phone = bookmark.phone,
+            address = bookmark.address,
+            notes = bookmark.notes
+        )
+    }
+
+    private fun mapBookmarkToBookmarkView(bookmarkId: Long) {
+        val bookmark = bookmarkRepo.getLiveBookmark(bookmarkId)
+        bookmarkDetailsView = Transformations.map(bookmark) {
+            bookmarkToBookmarkView(it)
+        }
+    }
+
+    fun getBookmark(bookmarkId: Long): LiveData<BookmarkDetailsView>? {
+        if (bookmarkDetailsView == null) {
+            mapBookmarkToBookmarkView(bookmarkId)
+        }
+        return bookmarkDetailsView
     }
 }
