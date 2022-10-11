70a67b1f119214c1d3b06530f9a5a9c15fb055ce
learnWithZak <learn.with.zak@gmail.com>
2022-10-11 05:52:14 +0200

    refactor: Retrieving the bookmark view

--- app/src/main/java/com/zak/placebook/ui/BookmarkDetailsActivity.kt
+++ app/src/main/java/com/zak/placebook/ui/BookmarkDetailsActivity.kt
@@ -1,12 +1,16 @@
 package com.zak.placebook.ui
 
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.zak.placebook.R
 import com.zak.placebook.databinding.ActivityBookmarkDetailsBinding
+import com.zak.placebook.viewmodel.BookmarkDetailsViewModel
 
 class BookmarkDetailsActivity : AppCompatActivity() {
     private lateinit var databinding: ActivityBookmarkDetailsBinding
+    private val bookmarkDetailsViewModel by viewModels<BookmarkDetailsViewModel>()
+    private val bookmarkDetailsView: BookmarkDetailsViewModel.BookmarkDetailsView? = null
 
     override fun onCreate(savedInstanceState: android.os.Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +20,14 @@ class BookmarkDetailsActivity : AppCompatActivity() {
 
     private fun setupToolbar() {
         setSupportActionBar(databinding.toolbar)
+    }
+
+    private fun populateImageView() {
+        bookmarkDetailsView?.let { bookmarkView ->
+            val placeImage = bookmarkView.getImage(this)
+            placeImage?.let {
+                databinding.imageViewPlace.setImageBitmap(placeImage)
+            }
+        }
     }
 }
