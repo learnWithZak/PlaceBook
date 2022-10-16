8f2d5254547fcdeb42efa70b26c4762815d2a95c
learnWithZak <learn.with.zak@gmail.com>
2022-10-16 13:32:40 +0200

    refactor: Updating the model

--- app/src/main/java/com/zak/placebook/db/PlaceBookDatabase.kt
+++ app/src/main/java/com/zak/placebook/db/PlaceBookDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.zak.placebook.model.Bookmark
 
-@Database(entities = [Bookmark::class], version = 2)
+@Database(entities = [Bookmark::class], version = 3)
 abstract class PlaceBookDatabase: RoomDatabase() {
 
     abstract fun bookmarkDao(): BookmarkDao
--- app/src/main/java/com/zak/placebook/model/Bookmark.kt
+++ app/src/main/java/com/zak/placebook/model/Bookmark.kt
@@ -15,7 +15,8 @@ data class Bookmark(
     var latitude: Double = 0.0,
     var longitude: Double = 0.0,
     var phone: String = "",
-    var notes: String = ""
+    var notes: String = "",
+    var category: String = ""
 ) {
     fun setImage(image: Bitmap, context: Context) {
         id?.let {
