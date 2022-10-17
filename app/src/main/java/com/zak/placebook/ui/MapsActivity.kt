e24cb0eaa9604865c20500755ba53a9da68db62f
learnWithZak <learn.with.zak@gmail.com>
2022-10-17 06:03:24 +0200

    refactor: Displaying categories on the map

--- app/src/main/java/com/zak/placebook/ui/MapsActivity.kt
+++ app/src/main/java/com/zak/placebook/ui/MapsActivity.kt
@@ -168,7 +168,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             Place.Field.PHONE_NUMBER,
             Place.Field.PHOTO_METADATAS,
             Place.Field.ADDRESS,
-            Place.Field.LAT_LNG
+            Place.Field.LAT_LNG,
+            Place.Field.TYPES
         )
         val request = FetchPlaceRequest.builder(placeId, placeFields).build()
         placesClient.fetchPlace(request)
@@ -242,8 +243,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .position(bookmark.location)
             .title(bookmark.name)
             .snippet(bookmark.phone)
-            .icon(BitmapDescriptorFactory.defaultMarker(
-                BitmapDescriptorFactory.HUE_AZURE))
+            .icon(bookmark.categoryResourceId?.let {
+                BitmapDescriptorFactory.fromResource(it)
+            })
             .alpha(0.8f))
 
         marker?.tag = bookmark
