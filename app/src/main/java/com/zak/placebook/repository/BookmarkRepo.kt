179ed9e95c09eb0b09a19569a80229a36e7c21f3
learnWithZak <learn.with.zak@gmail.com>
2022-10-17 05:48:56 +0200

    refactor: Converting place types

--- app/src/main/java/com/zak/placebook/repository/BookmarkRepo.kt
+++ app/src/main/java/com/zak/placebook/repository/BookmarkRepo.kt
@@ -2,6 +2,8 @@ package com.zak.placebook.repository
 
 import android.content.Context
 import androidx.lifecycle.LiveData
+import com.google.android.libraries.places.api.model.Place
+import com.zak.placebook.R
 import com.zak.placebook.db.PlaceBookDatabase
 import com.zak.placebook.model.Bookmark
 
@@ -9,6 +11,9 @@ class BookmarkRepo(context: Context) {
 
     private val db = PlaceBookDatabase.getInstance(context)
     private val bookmarkDao = db.bookmarkDao()
+    private var categoryMap: HashMap<Place.Type, String> = buildCategoryMap()
+    private var allCategories: HashMap<String, Int> = buildCategories()
+
     val allBookmarks: LiveData<List<Bookmark>>
         get() {
             return bookmarkDao.loadAll()
@@ -32,5 +37,52 @@ class BookmarkRepo(context: Context) {
 
     fun getBookmark(bookmarkId: Long): Bookmark {
         return bookmarkDao.loadBookmark(bookmarkId)
+    }
+
+    private fun buildCategoryMap() : HashMap<Place.Type, String> {
+        return hashMapOf(
+            Place.Type.BAKERY to "Restaurant",
+            Place.Type.BAR to "Restaurant",
+            Place.Type.CAFE to "Restaurant",
+            Place.Type.FOOD to "Restaurant",
+            Place.Type.RESTAURANT to "Restaurant",
+            Place.Type.MEAL_DELIVERY to "Restaurant",
+            Place.Type.MEAL_TAKEAWAY to "Restaurant",
+            Place.Type.GAS_STATION to "Gas",
+            Place.Type.CLOTHING_STORE to "Shopping",
+            Place.Type.DEPARTMENT_STORE to "Shopping",
+            Place.Type.FURNITURE_STORE to "Shopping",
+            Place.Type.GROCERY_OR_SUPERMARKET to "Shopping",
+            Place.Type.HARDWARE_STORE to "Shopping",
+            Place.Type.HOME_GOODS_STORE to "Shopping",
+            Place.Type.JEWELRY_STORE to "Shopping",
+            Place.Type.SHOE_STORE to "Shopping",
+            Place.Type.SHOPPING_MALL to "Shopping",
+            Place.Type.STORE to "Shopping",
+            Place.Type.LODGING to "Lodging",
+            Place.Type.ROOM to "Lodging"
+        )
+    }
+
+    fun placeTypeToCategory(placeType: Place.Type): String {
+        var category = "Other"
+        if (categoryMap.containsKey(placeType)) {
+            category = categoryMap[placeType].toString()
+        }
+        return category
+    }
+
+    private fun buildCategories(): HashMap<String, Int> {
+        return hashMapOf(
+            "Gas" to R.drawable.ic_gas,
+            "Lodging" to R.drawable.ic_lodging,
+            "Restaurant" to R.drawable.ic_restaurant,
+            "Shopping" to R.drawable.ic_shopping,
+            "Other" to R.drawable.ic_other,
+        )
+    }
+
+    fun getCategoryResourceId(plateCategory: String): Int? {
+        return allCategories[plateCategory]
     }
 }
